519446711d281acf7350f5c2d9d407fd719ae69e
Kenneth Mead <48263885+Xerner@users.noreply.github.com>
2025-01-28 14:48:23 -0500

    updated image dialog size

--- src/directives/image-dialog.directive.ts
+++ src/directives/image-dialog.directive.ts
@@ -59,8 +59,8 @@ export class ImageDialogDirective {
     };
     this.dialog.open(ImageDialogComponent, { 
       data: data,
-      maxWidth: "95vw",
-      maxHeight: "90vh",
+      maxWidth: "75%",
+      maxHeight: "75%",
       panelClass: "bg-transparent",
     });
   }
