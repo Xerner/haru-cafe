5aa1392c35a779e29ca2e91c2b64ac40aa927ba8
Kenneth Mead <kennethmead784@gmail.com>
2025-03-03 23:12:51 -0500

    added menu item image seeder

---
+++ haru_cafe_django/haru_seeder/seeders/README.md
@@ -0,0 +1,5 @@
+# Image Seeding
+
+Fuzzy search scoring
+- token_sort_ratio: above 80
+- ratio: above 60
--- haru_cafe_django/haru_seeder/seeders/image_url_seeder.py
+++ haru_cafe_django/haru_seeder/seeders/image_url_seeder.py
@@ -1,4 +1,12 @@
+from typing import Callable, List
+from thefuzz import fuzz
 from haru_cms.models import MenuItem
+from django.db.models import QuerySet
+import os
+
+FUZZ_RATIO_SCORE_THRESHOLD = 60
+FUZZ_TOKEN_SORT_RATIO_SCORE_THRESHOLD = 80
+FUZZ_TOKEN_SET_RATIO_SCORE_THRESHOLD = 80
 
 class ImageUrlSeeder:
   """
@@ -6,18 +14,43 @@ class ImageUrlSeeder:
   
   The JSON file should have a format similar to MenuItemRawJson
   """
-  def seed(self, image_dir: str):
-    print(f"Seeding image urls for all menu items in the database. image base url is", image_dir)
+  def seed(self, images_dir: str):
+    print(f"Seeding image urls for all menu items in the database. image base url is", images_dir)
     return_str = []
-    menu_items = MenuItem.objects.all()
-    for menu_item in menu_items:
-      is_menu_item_image_good = menu_item.image is not None or menu_item.image != ""
-      if is_menu_item_image_good:
-        continue
-      
-      menu_item.image = f"{menu_item.name.replace(' ', '-').lower()}.jpg"
-      menu_item.save()
-      return_str.append(f"Updated image for {menu_item.name} with {image_dir}/{menu_item.image}")
     
+    menu_items: QuerySet = MenuItem.objects.all()
+    self.seed_images(images_dir, menu_items, lambda menu_item: return_str.append(f"Seeded image for {menu_item.name} with {images_dir}/{menu_item.image}"))
     print("Seeding complete for image urls")
     return '\n'.join(return_str)
+  
+  def seed_images(self, images_dir: str, menu_items: QuerySet[MenuItem], on_menu_item_seeded: Callable[[MenuItem, bool], None]) -> List[str]:
+    image_files = os.listdir(images_dir)
+    for image_file in image_files:
+      self.seed_image(image_file, menu_items, on_menu_item_seeded)
+      
+  def seed_image(self, image_name: str, menu_items: QuerySet[MenuItem], on_menu_item_seeded: Callable[[MenuItem, bool], None]):
+    menu_item = self.find_matching_menu_item(image_name, menu_items)
+    if menu_item is None:
+      on_menu_item_seeded(menu_item, False)
+      return
+
+    menu_item.image = image_name
+    menu_item.save()
+    on_menu_item_seeded(menu_item, True)
+    
+  def find_matching_menu_item(self, image_name: str, menu_items: QuerySet[MenuItem]) -> MenuItem | None:
+    for menu_item in menu_items:
+      is_menu_item_image_bad = menu_item.image is None or menu_item.image == ""
+      if is_menu_item_image_bad:
+        continue
+      
+      if self.does_image_name_belong_to_menu_item(image_name, menu_item):
+        return menu_item
+      
+    return None
+    
+  def does_image_name_belong_to_menu_item(self, image_name: str, menu_item: MenuItem):
+    return fuzz.ratio(image_name, menu_item.name) > FUZZ_RATIO_SCORE_THRESHOLD \
+      or fuzz.token_set_ratio(image_name, menu_item.name) > FUZZ_TOKEN_SET_RATIO_SCORE_THRESHOLD \
+      or fuzz.token_sort_ratio(image_name, menu_item.name) > FUZZ_TOKEN_SORT_RATIO_SCORE_THRESHOLD
+    
